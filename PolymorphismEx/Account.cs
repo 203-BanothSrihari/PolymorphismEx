abb2055304dbb454886a5d998cd6effc10bcc121
sriharibanoth <sreeharibanoth1203@gmail.com>
2022-01-15 17:20:04 +0530

    Add project files.

---
+++ PolymorphismEx/Account.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PolymorphismEx
+{
+    class Account
+    {
+        public void account()
+        {
+            Console.WriteLine("account is created");
+        }
+        public void accName()
+        {
+            Console.WriteLine("account name is SBI Branch");
+        }
+        public virtual void AccountName()
+        {
+            Console.WriteLine("this is virtual method of account");
+        }
+    }
+    class Customer : Account
+    {
+        public void customer()
+        {       
+            Console.WriteLine("customer account is created");
+        }
+        public override void AccountName()
+        {
+            Console.WriteLine("account is overridden the customer");
+        }
+    }
+}
---
+++ PolymorphismEx/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace PolymorphismEx
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Account ac = new Customer();
+            ac.account();
+            ac.accName();
+            
+            ac.AccountName();
+            
+        }
+    }
+}
